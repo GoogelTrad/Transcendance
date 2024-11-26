3a82c39cf6c7d9db084d778fa926a95a76b30026
rgreiner1 <138033554+rgreiner1@users.noreply.github.com>
2024-11-26 14:34:02 +0100

    modif Dockerfile

--- frontend/core/src/Home.js
+++ frontend/core/src/Home.js
@@ -1,19 +1,15 @@
 
 import "./App.css"
 
+
+
 function Home()
 {
     return (
-        <div>
-            <h1>HOME</h1>
-            {user ? (
-                <div>   
-                    <p>Nom : {user.name}</p>
-                    <p>Email : {user.email}</p>
-                </div>
-            ) : (
-                <p>Aucun utilisateur trouvé.</p>
-            )}
+        <div className="home-container">
+            <div className="welcome-container">
+                <h1>Welcome to Transcendance !</h1>
+            </div>
         </div>
     );
 }
